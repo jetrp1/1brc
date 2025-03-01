cf0021271fa944d746531a58a3631b4936623664
Ryan Peel <ryan.l.peel@gmail.com>
2025-02-28 23:43:19 -0600

    modified:   calculate_average_baseline.py

--- calculate_average_baseline.py
+++ calculate_average_baseline.py
@@ -1,9 +1,40 @@
 import sys
 
 def calc_average(file_name):
-    with open(file_name, 'r') as dataset:
-        
+    result_avg = {}
+    result_min = {}
+    result_max = {}
+    result_count = {}
+    station_names = set()
 
+    with open(file_name, 'r') as dataset:
+        for record in dataset:
+            station, value = record.split(',')
+
+            if station not in station_names:
+                station_names.add(station)
+                result_avg[station] = value
+                result_min[station] = value
+                result_max[station] = value
+                result_count[station] = 1
+
+            result_count[station] += 1
+            result_avg[station] += value
+
+            if result_max[station] < value:
+                result_max[station] = value
+
+            if result_min[station] > value:
+                result_min[station] = value
+    
+    results = '{'
+
+    for station in station_names:
+        results.append(f'{station}={result_min[station]}/{result_avg[station]/result_count[station]:.1f}/{result_max[station]}, ')
+
+    results.append('}')
+
+    print(results)        
 
 
 if __name__ == '__main__':
