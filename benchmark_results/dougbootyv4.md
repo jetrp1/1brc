8577af4576d92e16fdcbf7440a390a1f02f106cb
Ryan Peel <ryan.l.peel@gmail.com>
2025-03-17 16:40:22 -0500

    modified:   1BRC.MD 	new file:   benchmark_results/dougbooty_v4.py 	new file:   benchmark_results/dougbootyv4.md 	modified:   setup_env.sh

---
+++ benchmark_results/dougbootyv4.md
@@ -0,0 +1,3 @@
+| Command | Mean [s] | Min [s] | Max [s] | Relative |
+|:---|---:|---:|---:|---:|
+| `pypy3 ./dougbooty_v4.py` | 123.622 ± 0.722 | 122.856 | 124.292 | 1.00 |
--- setup_env.sh
+++ setup_env.sh
@@ -3,7 +3,7 @@
 DISK_PATH='/media/ramDisk'
 
 sudo mkdir -p $DISK_PATH
-sudo mount -t tmpfs -o size=16G tmpfs $DISK_PATH
+sudo mount -t tmpfs -o size=24G tmpfs $DISK_PATH
 
 
 
